2ba2428c31d67a2d567c0c11cdf99c39bb8c518a
Alexis Puga <alexispuga@pm.me>
2021-03-02 04:51:00 -0600

    ENH: Run from CLI
    ENH: Add src/index.js
    
    Export a function.
    
    ENH: Add bin/cli
    
    Run the exported function from /src
    
    TST: Test cli command
    
    ENH: Add test/integration/cli.test.js

---
+++ test/integration/cli.test.js
@@ -0,0 +1,12 @@
+const { promisify } = require('util')
+const { execFile } = require('child_process')
+const execFileAsync = promisify(execFile)
+
+describe('precise-watcher', () => {
+  it('Should work with defaults', async () => {
+    const { stderr, stdout } = await execFileAsync('bin/cli')
+
+    expect(stderr).toBe('')
+    expect(stdout).toBe('')
+  })
+})
