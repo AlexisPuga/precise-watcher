6ba383034c62c2995e5483005ff86662e3f169ba
Alexis Puga <alexispuga@pm.me>
2021-03-02 09:05:21 -0600

    ENH: Read "src" property in config
    This commit adds support for a lot of things because all the passed options
    to another modules should be allowable to be modified from the config file.
    For example, we need cwd for child_process.exec, but also for chokidar files.
    So, setting options in two commits to separate those things gets more
    complicated.. Anyway, a config could be passed like so: {
      "precise-watcher": {
        // expects an object to pass global chokidar options.
        "chokidar": {},
        // expects an array, but should handle any value.
        "src": [{
          // expects an array, but should handle any value.
          "pattern": [/* glob pattern */],
          // expects an string. Replaces src.regexp matches.
          "run": "echo <file> && echo <file>", /** example */
          // expects an object to pass local chokidar options.
          "chokidar": {},
          // expects any chokidar event, like "add", "change", ...
          // defaults to "change".
          "on": "change",
          // expects and string or a regexp to replace tokens in your command.
          "regexp": /<file>/g
        }]
      }
    }
    
    TST: Add integration test for "src" property in integration/src.test.js
    
    TST: Set a valid config in 1st test in integration/src.test.js
    
    A boolean is not a valid config. Set an object instead.
    
    ENH: .gitignore temp*

--- src/index.js
+++ src/index.js
@@ -1,19 +1,75 @@
+const path = require('path')
+const chokidar = require('chokidar')
 const readConfig = require('./lib/read-config')
+const execAsync = require('./lib/exec-async')
+const handleEvent = (eventName, command, {
+  regexp = /<file>/g,
+  cmd: execAsyncOptions
+}) => {
+  const pathNthArgs = {
+    add: 0,
+    change: 0,
+    unlink: 0,
+    addDir: 0,
+    unlinkDir: 0,
+    raw: 1
+  }
+  const pathNthArg = pathNthArgs[eventName]
+
+  return async (...args) => {
+    const path = args[pathNthArg]
+    const cmd = command.replace(regexp, path)
+
+    await execAsync(cmd, execAsyncOptions)
+  }
+}
 
 module.exports = (options) => {
   const {
-    cwd,
-    config: configFilename = 'package.json',
-  } = Object(options);
-  let config = readConfig(configFilename, {cwd})
+    cwd: userDirectory = process.cwd(),
+    config: configFilename = 'package.json'
+  } = Object(options)
+  let config = readConfig(configFilename, {
+    cwd: userDirectory
+  })
 
   if (configFilename === 'package.json') {
     config = Object(config)['precise-watcher']
   }
 
-  if (!config) {
-    return false
+  if (config) {
+    const { src, chokidar: chokidarConfig } = Object(config)
+    const globalChokidarOptions = Object(chokidarConfig)
+    const sources = (Array.isArray(src) ? src : [src]).filter(
+      (src) => Boolean(src)
+    )
+
+    sources.forEach((value) => {
+      const { pattern, run, on, regexp, chokidar: localChokidarOptions } = Object(value)
+      const chokidarOptions = {
+        ...globalChokidarOptions,
+        ...localChokidarOptions
+      }
+      const src = (Array.isArray(pattern) ? pattern : [pattern]).map(
+        (src) => path.join(userDirectory, src)
+      )
+      const watcher = chokidar.watch(src, chokidarOptions)
+      const eventNames = Array.isArray(on) ? on : [on]
+
+      eventNames.forEach((eventName) => {
+        if (typeof eventName !== 'string') {
+          eventName = 'change'
+        }
+
+        watcher.on(eventName, handleEvent(eventName, run, {
+          regexp,
+          cmd: { cwd: userDirectory }
+        }))
+      })
+    })
+
+    return true
   }
 
-  return true
+  return false
 }
--- test/integration/src.test.js
+++ test/integration/src.test.js
@@ -1,4 +1,16 @@
+const fs = require('fs')
+const path = require('path')
+const mockJson = (filepath, json) => jest.doMock(filepath, () => (json), {
+  virtual: true
+})
+const wait = async (ms) => new Promise((resolve) => {
+  setTimeout(resolve, ms)
+})
+
+jest.mock('child_process')
+
 describe('/src', () => {
+  const { exec } = require('child_process')
   const preciseWatcher = require('../../src')
 
   beforeEach(() => {
@@ -8,10 +20,44 @@ describe('/src', () => {
   it('Should read config from "precise-watcher" property located in ' +
     'users\' package.json, by default', async () => {
     // Should return early (returning false).
-    jest.doMock('../../package.json', () => ({
-      'precise-watcher': true
-    }), { virtual: true })
+    mockJson('../../package.json', {
+      'precise-watcher': {}
+    })
 
     expect(preciseWatcher()).toBe(true)
+  })
+
+  it('Should read given sources', async (done) => {
+    const destinationFile = path.join(process.cwd(), 'test.temp')
+    let calls = 0
+
+    exec.mockImplementation((cmd, options) => {
+      expect(cmd).toBe(`echo ${destinationFile}`)
+      calls++
+
+      if (calls === 2) {
+        fs.unlinkSync(destinationFile)
+        done()
+      }
+    })
+    mockJson('../../package.json', {
+      'precise-watcher': {
+        src: [{
+          pattern: '*.temp',
+          run: 'echo <file>',
+          chokidar: { interval: 1 }
+        }]
+      }
+    })
+
+    // Write initial file. This file shouldn't be handled by chokidar.
+    fs.writeFileSync(destinationFile, '1')
+
+    preciseWatcher()
+
+    await wait(100)
+    fs.writeFileSync(destinationFile, '2')
+    await wait(100)
+    fs.writeFileSync(destinationFile, '3')
   })
 })
