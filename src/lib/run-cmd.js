1614cf7d36065352797f95dcfd5226c5f5ceae54
Alexis Puga <alexispuga@pm.me>
2021-03-03 20:22:07 -0600

    ENH: Add src/lib/run-cmd.js

---
+++ src/lib/run-cmd.js
@@ -0,0 +1,22 @@
+const { spawn } = require('child_process')
+const log = console.log
+const logError = console.error
+
+module.exports = async (cmd, args, options) => await new Promise((resolve, reject) => {
+  const child = spawn(cmd, args, options)
+  const { stdout, stderr } = child
+
+  log(`Running ${cmd}`)
+
+  stdout.on('data', (data) => {
+    log(data.toString())
+  })
+
+  stderr.on('data', (data) => {
+    logError(data.toString())
+  })
+
+  child.on('exit', (status) => {
+  	resolve(status)
+  })
+})
