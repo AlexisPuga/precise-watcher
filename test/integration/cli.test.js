3800ddd6cfa8ac658f5654a18fae78d5487c6f5d
Alexis Puga <alexispuga@pm.me>
2021-03-05 05:37:21 -0600

    Revert "ENH: Set AbortController's signal in test/integration/cli.test.js"
    This reverts commit dbd1958dc47cf41cd125297a0c73df04e3605466.

--- test/integration/cli.test.js
+++ test/integration/cli.test.js
@@ -3,23 +3,8 @@ const { execFile } = require('child_process')
 const execFileAsync = promisify(execFile)
 
 describe('precise-watcher', () => {
-  const run = async () => await execFileAsync('bin/cli', {
-    signal
-  })
-  let abortController
-  let signal
-
-  beforeEach(() => {
-    abortController = new global.AbortController()
-    signal = abortController.signal
-  })
-
-  afterEach(() => {
-    abortController.abort()
-  })
-
   it('Should work with defaults', async () => {
-    const { stderr, stdout } = await run()
+    const { stderr, stdout } = await execFileAsync('bin/cli')
 
     expect(stderr).toBe('')
     expect(stdout).toBe('Stopping precise-watcher...\n')
