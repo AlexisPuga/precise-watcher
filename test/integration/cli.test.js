dbd1958dc47cf41cd125297a0c73df04e3605466
Alexis Puga <alexispuga@pm.me>
2021-03-05 04:32:47 -0600

    ENH: Set AbortController's signal in test/integration/cli.test.js
    Handle logic on describe(), using beforeEach() and afterEach().

--- test/integration/cli.test.js
+++ test/integration/cli.test.js
@@ -3,8 +3,23 @@ const { execFile } = require('child_process')
 const execFileAsync = promisify(execFile)
 
 describe('precise-watcher', () => {
+  const run = async () => await execFileAsync('bin/cli', {
+    signal
+  })
+  let abortController
+  let signal
+
+  beforeEach(() => {
+    abortController = new global.AbortController()
+    signal = abortController.signal
+  })
+
+  afterEach(() => {
+    abortController.abort()
+  })
+
   it('Should work with defaults', async () => {
-    const { stderr, stdout } = await execFileAsync('bin/cli')
+    const { stderr, stdout } = await run()
 
     expect(stderr).toBe('')
     expect(stdout).toBe('Stopping precise-watcher...\n')
