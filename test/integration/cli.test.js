8a9543c98b30d2e1e734eb2649e414f5072937ae
Alexis Puga <alexispuga@pm.me>
2021-03-07 23:19:33 -0600

    ENH: Handle CLI options using yargs
    Describe usage, the default command, and the available options.
    Run start() when no commands are present.
    
    TST: Add basic test for all known options in test/integration/cli.test.js
    
    ENH: Add test/fixtures/precise-watcher.config.js

--- test/integration/cli.test.js
+++ test/integration/cli.test.js
@@ -9,4 +9,23 @@ describe('precise-watcher', () => {
     expect(stderr).toBe('')
     expect(stdout).toBe('Stopping precise-watcher...\n')
   })
+
+  test.each([
+    [[]],
+    [['start']]
+  ])('Should handle all known options.', async (args) => {
+    const { stderr, stdout } = await execFileAsync('bin/cli', args.concat([
+      '--cwd',
+      __dirname,
+      '--config',
+      '../fixtures/precise-watcher.config.js'
+    ]))
+
+    // @TODO Test debugging.
+
+    expect(stderr).toBe('')
+    expect(stdout).toBe([
+      'Stopping precise-watcher...'
+    ].join('\n') + '\n')
+  })
 })
