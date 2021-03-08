e1fa24206135435e82c22dfa736155d886eabe9b
Alexis Puga <alexispuga@pm.me>
2021-03-08 02:31:09 -0600

    MAINT: Set "watchers" param optional in src/stop.js' main function
    Handle optional "watchers" param in src/stop.js:
    - Move code from src/shutdown.js to src/stop.js

--- src/shutdown.js
+++ src/shutdown.js
@@ -1,17 +1,8 @@
 const debug = require('debug')('precise-watcher')
-const allWatchers = require('./watchers')
 const stop = require('./stop')
-const log = console.log
 const logError = console.error
 
 module.exports = (watchers) => {
-  log('Stopping precise-watcher...')
-
-  if (!watchers) {
-    debug('Removing all watchers.')
-    watchers = allWatchers
-  }
-
   stop(watchers).then(() => {
     debug('Setting process.exitCode to 0.')
     process.exitCode = 0
--- src/stop.js
+++ src/stop.js
@@ -1,7 +1,17 @@
+const debug = require('debug')('precise-watcher')
+const allWatchers = require('./watchers')
+const log = console.log
 const removeAllWatchers = async (watchers) => await Promise.all(
   watchers.map(async (watcher) => await watcher.close())
 )
 
 module.exports = async (watchers) => {
+  log('Stopping precise-watcher...')
+
+  if (!watchers) {
+    debug('Removing all watchers.')
+    watchers = allWatchers
+  }
+
   return await removeAllWatchers(watchers)
 }
